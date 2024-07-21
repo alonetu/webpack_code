b5c4a25e733d01d4ab9a38bbfddd0a55448d7a74
Alonetu <1217842903@qq.com>
2024-07-21 19:03:08 +0800

    feat: webpack high config

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -1,2 +1,2 @@
-!function(){"use strict";console.log(4-2),console.log(function(...o){return o.reduce(((o,n)=>o+n),0)}(1,2,3,4))}();
+!function(){"use strict";console.log(3*3),console.log(function(o,n){return o-n}(4,2)),console.log(function(...o){return o.reduce(((o,n)=>o+n),0)}(1,2,3,4))}();
 //# sourceMappingURL=main.js.map
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import count from './js/count';
 import sum from './js/sum';
+import { mul } from './js/math';
 // 想要webpack打包资源，必须引入该资源
 import "./css/index.css";
 import "./less/index.less";
 import "./scss/index.scss";
 
+console.log(mul(3, 3)); 
 console.log(count(4, 2));
 console.log(sum(1, 2, 3, 4));
